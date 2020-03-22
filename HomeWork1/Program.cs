5a07e93019286fa8a56625115ff3e3868d980e58
Viktor <vitalineage22@gmail.com>
2020-03-22 18:06:39 +0300

    feat: Started new sln

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dear friend!");
+            Console.WriteLine("Let's start SimpleCalc program!");
+        }
+    }
+}
